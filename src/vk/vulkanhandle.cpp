237774b6507e5fa4618451dcc71cc98770b7eb4a
Kieran <kieranknowles11@hotmail.co.uk>
2025-12-26 01:38:33 +0000

    disable verbose vma log by default

--- src/vk/vulkanhandle.cpp
+++ src/vk/vulkanhandle.cpp
@@ -8,11 +8,20 @@
 #include <fmt/base.h>
 #include <vulkan/vulkan_core.h>
 
-#define VMA_DEBUG_LOG_FORMAT(format, ...)                                      \
+#define VMA_LOG(preface, format, ...)                                          \
   do {                                                                         \
-    printf((format), __VA_ARGS__);                                             \
-    printf("\n");                                                              \
+    printf(preface format "\n", __VA_ARGS__);                                  \
   } while (false)
+
+// Always log leaks
+#define VMA_LEAK_LOG_FORMAT(format, ...)                                       \
+  VMA_LOG("[vma: leak] ", format, __VA_ARGS__)
+
+// Don't log allocations unless requested, as these are very verbose
+#ifdef VN_LOGALLOCATIONS
+#define VMA_DEBUG_LOG_FORMAT(format, ...)                                      \
+  VMA_LOG("[vma: info] ", format, __VA_ARGS__)
+#endif
 
 #define VMA_IMPLEMENTATION
 #include <vk_mem_alloc.h>
