ffedb7d3843742f7ff95954d29e1fda0594febd0
Kieran <kieranknowles11@hotmail.co.uk>
2026-01-03 15:03:57 +0000

    include stb impl

---
+++ src/impl.cpp
@@ -0,0 +1,22 @@
+// Implement header-only libraries
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+#define VMA_LOG(preface, format, ...)                                          \
+  do {                                                                         \
+    printf(preface format "\n", __VA_ARGS__);                                  \
+  } while (false)
+
+// Always log leaks
+#define VMA_LEAK_LOG_FORMAT(format, ...)                                       \
+  VMA_LOG("[vma: leak] ", format, __VA_ARGS__)
+
+// Don't log allocations unless requested, as these are very verbose
+#ifdef VN_LOGALLOCATIONS
+#define VMA_DEBUG_LOG_FORMAT(format, ...)                                      \
+  VMA_LOG("[vma: info] ", format, __VA_ARGS__)
+#endif
+
+#define VMA_IMPLEMENTATION
+#include <vk_mem_alloc.h>
--- src/vk/vulkanhandle.cpp
+++ src/vk/vulkanhandle.cpp
@@ -8,24 +8,6 @@
 #include <fmt/base.h>
 #include <vulkan/vulkan_core.h>
 
-#define VMA_LOG(preface, format, ...)                                          \
-  do {                                                                         \
-    printf(preface format "\n", __VA_ARGS__);                                  \
-  } while (false)
-
-// Always log leaks
-#define VMA_LEAK_LOG_FORMAT(format, ...)                                       \
-  VMA_LOG("[vma: leak] ", format, __VA_ARGS__)
-
-// Don't log allocations unless requested, as these are very verbose
-#ifdef VN_LOGALLOCATIONS
-#define VMA_DEBUG_LOG_FORMAT(format, ...)                                      \
-  VMA_LOG("[vma: info] ", format, __VA_ARGS__)
-#endif
-
-#define VMA_IMPLEMENTATION
-#include <vk_mem_alloc.h>
-
 namespace selwonk::vulkan {
 VkBool32 VulkanHandle::debugCallback(
     VkDebugUtilsMessageSeverityFlagBitsEXT severity,
