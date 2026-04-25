b8b55a5bc926a33ace1ca09832adc9ef75eda37e
Kieran <kieranknowles11@hotmail.co.uk>
2026-04-25 23:14:35 +0100

    fix ringbuffer reading garbage data on init

--- src/vncore/vncore/ringbuffer.hpp
+++ src/vncore/vncore/ringbuffer.hpp
@@ -9,19 +9,22 @@ template <typename T, size_t Count> class RingBuffer {
 public:
   void record(T sample) {
     mSamples[mIndex] = sample;
+    mKnownSamples++;
     mIndex = (mIndex + 1) % mSamples.size();
   }
 
-  T average() {
+  T average() const {
     T sum{};
-    for (auto& s : mSamples) {
-      sum += s;
+    for (int i = 0; i < std::min(mSamples.size(), mKnownSamples); i++) {
+      sum += mSamples[i];
     }
+
     return sum / Count;
   }
 
 private:
   size_t mIndex = 0;
+  size_t mKnownSamples = 0;
   std::array<T, Count> mSamples;
 };
 } // namespace selwonk::core
