2b62d1e3f833de4455cbd4bf3292598851df49f1
Kieran <kieranknowles11@hotmail.co.uk>
2026-05-03 21:42:59 +0100

    add missing imgui sources

--- cmake/vn_targets.cmake
+++ cmake/vn_targets.cmake
@@ -60,11 +60,13 @@ function(vn_common_options TARGET)
 endfunction()
 
 function(vn_add_executable TARGET)
+  # TODO: Add headers for visual studio
   add_executable(${TARGET} ${ARGN})
   vn_common_options(${TARGET})
 endfunction()
 
 function(vn_add_library TARGET)
+  # TODO: Add headers for visual studio
   add_library(${TARGET} ${ARGN})
   vn_common_options(${TARGET})
 endfunction()
