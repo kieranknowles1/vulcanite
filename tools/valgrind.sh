ebf99177cc511b8f66c1fd990c0c7d43af4ebac6
Kieran <kieranknowles11@hotmail.co.uk>
2026-06-14 00:20:16 +0100

    Add valgrind suppressions file

--- tools/valgrind.sh
+++ tools/valgrind.sh
@@ -2,7 +2,9 @@
 set -euo pipefail
 
 JOBS=8
+TOOLS=$(dirname "${BASH_SOURCE}")
 
 make -C build -j$JOBS vulcanite
 
-exec valgrind --leak-check=full ./build/vulcanite debug.quit_after 1 core.data_directory build/assets
+exec valgrind --leak-check=full --suppressions=$TOOLS/valgrind.supp --gen-suppressions=all \
+  ./build/vulcanite debug.quit_after 1 core.data_directory build/assets
